5c44edbd7b9c88c15b44e4d776edf4882d39a034
Elena Brazhenko <elenabrazhenko34@gmail.com>
2022-05-14 18:18:12 +0300

    popup js

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,33 @@
-let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__close-icon');
+let editButton = document.querySelector(".profile__edit-button");
+let popup = document.querySelector(".popup");
+let closeButton = document.querySelector(".popup__close-icon");
+let formElement = document.querySelector(".popup__container");
+let nameInput = document.querySelector(".popup__input_type_name");
+let jobInput = document.querySelector(".popup__input_type_job");
+let profileName = document.querySelector(".profile__title");
+let profileJob = document.querySelector(".profile__subtitle");
 
-function openForm() {
-  popup.classList.add('popup_opened');
+function toggleForm() {
+  popup.classList.toggle("popup_opened");
+
+  if (popup.classList.contains("popup_opened")) {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+  } else {
+    nameInput.value = "";
+    jobInput.value = "";
+  }
 }
 
-function closePopup() {
-    popup.classList.remove('popup_opened');
+editButton.addEventListener("click", toggleForm);
+closeButton.addEventListener("click", toggleForm);
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  toggleForm();
 }
 
-editButton.addEventListener('click', openForm);
-
-closeButton.addEventListener('click', closePopup);
+formElement.addEventListener("submit", formSubmitHandler);
