eb757479821deb98b2f72394938bb8442de85a4a
Elena Brazhenko <elenabrazhenko34@gmail.com>
2022-06-04 14:11:36 +0300

    initial cards

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,7 @@ let jobInput = document.querySelector(".popup__input_type_job");
 let profileName = document.querySelector(".profile__title");
 let profileJob = document.querySelector(".profile__subtitle");
 
+
 function toggleForm() {
   popup.classList.toggle("popup_opened");
 
@@ -27,7 +28,52 @@ function formSubmitHandler(evt) {
   toggleForm();
 }
 
-formElement.addEventListener("submit", formSubmitHandler);
 
+formElement.addEventListener("submit", formSubmitHandler);
 editButton.addEventListener("click", toggleForm);
 closeButton.addEventListener("click", toggleForm);
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
+const elements = document.querySelector('.elements');
+
+function addCards (element) {
+
+  const container = document.querySelector('#container').content;
+  const card = container.querySelector('.elements__item').cloneNode(true);
+
+  card.querySelector('.elements__title').textContent = element.name;
+  card.querySelector('.elements__image').src = element.link;
+
+  elements.append(card);
+};
+
+
+initialCards.forEach(addCards);
